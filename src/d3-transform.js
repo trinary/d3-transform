2f9dd530d207dc099e1930ea38d28439806dab0d
Erik Cunningham <djtrinary@gmail.com>
2013-03-12 14:45:35 -0600

    simple things maybe working, function eval next.

--- src/d3-transform.js
+++ src/d3-transform.js
@@ -1,7 +1,22 @@
 (function () {
   d3.selection.prototype.translate = function (x,y) {
-    console.log(this,x,y);
-    this.each
+    if (arguments.length < 2) {
+    }
+    console.log(this.node(),x,y);
+    this.each(d3_transform_attr("translate",x,y));
     return this;
   };
+  function d3_transform_attr(type) {
+    var args = Array.prototype.slice.call(arguments);
+    var values  = [];
+    for (var i = 0 ; var m = ++i; i< args.length) { 
+      if(typeof args[i] === "function") {
+        values[i] = args[i]
+      }
+    }
+    args.shift();
+    console.log(args);
+    function transformConstant() { this.setAttribute("transform", type + "(" + args + ") "); }
+    function transformFunction() { var x = arg }
+  }
 })();
