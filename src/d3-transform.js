794cb60372515943256c5ca6f2d9ae2ba57ab0be
Erik Cunningham <djtrinary@gmail.com>
2013-03-29 23:49:36 -0600

    split up func def and return logic

--- src/d3-transform.js
+++ src/d3-transform.js
@@ -4,25 +4,30 @@
   }
 
   function d3_selection(name, value1, value2) {
-    if(typeof value1 === "object" && value1.length) {
-      return function() {
+
+    var transforn_array = function() {
         var existing = this.getAttribute("transform") || ""
         console.log (existing, name, value1.join(","));
         this.setAttribute("transform",existing + name + "(" + value1.join(",") + ")")
-      }
     }
-    if(typeof value1 === "function") {
-      return function() {
+    var transform_function = function() {
         console.log(arguments);
         var x = value1.apply(this,arguments);
         var existing = this.getAttribute("transform") || ""
         console.log (existing, name, x);
         this.setAttribute("transform",existing + name + "(" + x.join(",") +  ")")
-      }
     }
-    return function() {
+    var transform_static = function() {
       var existing = this.getAttribute("transform") || ""
       this.setAttribute("transform",existing + name + "(" + value1 + "," + value2 + ")")
     }
+
+    if(typeof value1 === "object" && value1.length) {
+      return transform_array;
+    }
+    if(typeof value1 === "function") {
+      return transform_function;
+    }
+    return transform_static;
   }
 })();
